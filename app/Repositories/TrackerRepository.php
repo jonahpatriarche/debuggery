743128498e25c73fed03189493fa77fda5ea32e1
Jonah <Jonah@craftt.com>
2017-02-28 15:30:01 -0800

    TRACKERS: 	- added Controller skeleton 	- added Repository and RepositoryInterface skeletons

---
+++ app/Repositories/TrackerRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repositories;
+
+class TrackerRepository extends RepositoryAbstract implements TrackerRepositoryInterface
+{
+
+    /**
+     * Return a collection of all trackers in storage
+     *
+     * @return mixed
+     */
+    public function all()
+    {
+        // TODO: Implement all() method.
+    }
+
+    /**
+     * @param $id
+     *
+     * @return mixed
+     */
+    public function find($id)
+    {
+        // TODO: Implement find() method.
+    }
+
+    /**
+     * @param $id
+     *
+     * @return mixed
+     */
+    public function delete($id)
+    {
+        // TODO: Implement delete() method.
+    }
+}
