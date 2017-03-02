3efc163d1713926e1910cb1cae0819ddacc3fd15
Jonah <Jonah@craftt.com>
2017-03-02 14:05:58 -0800

    improved BuggerTransformer, add documentation, add more transformer tests

--- app/Transformers/BuggerTransformer.php
+++ app/Transformers/BuggerTransformer.php
@@ -17,49 +17,17 @@ class BuggerTransformer extends BaseEloquentTransformer implements TransformerIn
      */
     public function item($item)
     {
-        $str = $item['message'];
+        $log_message = $item['message'];
 
-        /**
-         * Check for stack trace
-         */
-        if (str_contains($str, 'Stack trace')) {
-            list($error, $trace_string) = explode('Stack trace:', $str);
-            $trace_lines = explode('#', $trace_string);
-            $trace = $this->transformTrace($trace_lines);
-        }
-        else {
-            $error = $str;
-            $trace = [];
-        }
+        list($message, $trace) = $this->getStackTrace($log_message);
+        list($error_message, $file) = $this->getErrorFile($message);
+        list($err_msg_class, $err_msg_body) = $this->getErrorClassName($error_message);
 
-        /**
-         * Check for file name
-         *  ex: 'DeveloperException in app/BadCode.php(22)...'
-         */
-        if (str_contains($error, ' in ')) {
-            list($msg, $file) = explode(' in ', $error);
-        }
-        else {
-            $msg = $error;
-            $file = null;
-        }
-
-        /**
-         * Check for Error class
-         *  ex: 'BadMethodCallException: blah blah something blew up blah blah blah...'
-         */
-        if (str_contains($msg, ':')) {
-            list($class, $error) = explode(':', $msg);
-        }
-        else {
-            $class = 'Unknown';
-            $error = $msg;
-        }
 
         /**
          * Extract the name of the class from the end of the error path
          */
-        $class = last(explode('\\', $class));
+        $err_msg_class = last(explode('\\', $err_msg_class));
 
         /**
          * Strip the base path off of the file name
@@ -67,8 +35,8 @@ class BuggerTransformer extends BaseEloquentTransformer implements TransformerIn
         $file = $this->stripBasePath($file);
 
         $response = [
-            'error_class' => $class,
-            'message'     => $error,
+            'error_class' => $err_msg_class,
+            'message'     => $err_msg_body,
             'file'        => $file,
             'trace'       => $trace,
             'level_name'  => strtolower($item['level_name']),
@@ -79,6 +47,66 @@ class BuggerTransformer extends BaseEloquentTransformer implements TransformerIn
         ];
 
         return $response;
+    }
+
+    /**
+     * Split message into error message and file name using ' in ' as default delimiter
+     *  - if delimiter not present, set file to null and use entire message as error message
+     *
+     * @param string $message - 'DeveloperException: You did something super wrong in app/BadCode.php(22)...'
+     */
+    private function getErrorFile($message, $delimiter = ' in ')
+    {
+        if (str_contains($message, $delimiter)) {
+            list($error_message, $file) = explode($delimiter, $message);
+        }
+        else {
+            $error_message = $message;
+            $file          = null;
+        }
+
+        return [$error_message, $file];
+    }
+
+    /**
+     * Split the error class from the error message body using ': ' as delimiter
+     *  - if delimiter not present, set both class and msg to the whole message
+     *
+     * @param $message - 'BadMethodCallException: blah blah something blew up blah blah blah...'
+     *
+     * @return array
+     */
+    private function getErrorClassName($message, $delimiter = ': ')
+    {
+        if (str_contains($message, $delimiter)) {
+            list($err_msg_class, $err_msg_body) = explode($delimiter, $message);
+        }
+        else {
+            $err_msg_class = $message;
+            $err_msg_body  = $message;
+        }
+
+        return [$err_msg_class, $err_msg_body];
+    }
+
+    /**
+     * Split log message into error message and stack trace using 'Stack trace: ' as delimiter
+     *  - trace is returned as an array of strings that represent each line of the stack trace
+     *  - if delimiter not present, use the entire log message and set trace to empty array
+     */
+    private function getStackTrace($log_message, $delimiter = 'Stack trace: ')
+    {
+        if (str_contains($log_message, $delimiter)) {
+            list($message, $trace_string) = explode($delimiter, $log_message);
+            $trace_lines = explode('#', $trace_string);
+            $trace       = $this->transformTraceLines($trace_lines);
+        }
+        else {
+            $message = $log_message;
+            $trace   = [];
+        }
+
+        return [$message, $trace];
     }
 
     /**
@@ -116,38 +144,15 @@ class BuggerTransformer extends BaseEloquentTransformer implements TransformerIn
          * - explode string around base path
          * - remove base path
          * - stick string back together
+         * - remove spaces and white space characters from either end of string
          */
         if (str_contains($path, base_path())) {
             $str_array = explode(base_path(), $path);
             array_forget($str_array, base_path());
-            $path = implode($str_array);
+            $path = rtrim(implode($str_array), ' ');
         }
 
         return $path;
-    }
-
-    /**
-     * Use delimiter to split lines of trace into arrays of trimmed strings
-     *
-     * @param $trace_lines
-     *
-     * @return array
-     */
-    private function transformTrace($trace_lines, $delimiter = ':')
-    {
-        $trace = [];
-        foreach ($trace_lines as $line) {
-            $line = explode($delimiter, $line);
-            $line = $this->stripBasePath($line);
-
-            // Skip empty arrays and empty strings
-            if (sizeof($line) > 0) {
-                array_push($trace, $line);
-            }
-
-        }
-
-        return $trace;
     }
 
     /**
@@ -164,5 +169,31 @@ class BuggerTransformer extends BaseEloquentTransformer implements TransformerIn
         return Carbon::parse($item['created_at'])
             ->timezone($timezone)
             ->toDayDateTimeString();
+    }
+
+    /**
+     * Tidy up lines of stack trace
+     *  - remove empty lines
+     *  - remove all instances of base path
+     *  - trim whitespace off start and end of string
+     *
+     * @param $trace_lines
+     *
+     * @return array
+     */
+    private function transformTraceLines($trace_lines)
+    {
+        $trace = [];
+        foreach ($trace_lines as $line) {
+            $line = $this->stripBasePath($line);
+
+            // Skip empty arrays and empty strings
+            if (sizeof($line) > 0) {
+                array_push($trace, $line);
+            }
+
+        }
+
+        return $trace;
     }
 }
