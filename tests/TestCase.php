3ba24a512816990bfcaac83c7c6420bf9c388f21
Jonah <Jonah@craftt.com>
2017-03-02 16:58:09 -0800

    fixed issues in transformers, added more tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,23 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, DatabaseTransactions;
+
+    /**
+     * Assert test fails and print error message and line
+     *
+     * @param \Exception $e
+     */
+    protected function failWithException(\Exception $e)
+    {
+        $this->assertTrue(
+            false,
+            'Transformer threw exception: ' . $e->getMessage() . ' at line ' . $e->getLine()
+        );
+    }
 }
--- tests/Unit/Transformers/BaseTransformerTest.php
+++ tests/Unit/Transformers/BaseTransformerTest.php
@@ -2,7 +2,102 @@
 
 namespace Tests\Unit;
 
-class BaseTransformerTest
+use App\Bugger;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Log;
+use Tests\TestCase;
+
+abstract class BaseTransformerTest extends TestCase
 {
 
+    /**
+     * Write a log entry with components specified by boolean parameters in standard error log format
+     *  Example:
+     *      '\path\to\ClassName: an error occurred in \path\to\file:[insert line number]. Stack trace:
+     *          #1 line of stack trace
+     *          #2 another line of stack trace...'
+     *
+     * @param string|null $level      - desired log level
+     * @param string|null $message    - log message. If null, no message will be generated
+     * @param string|null $class      - when given, prefix message with path to exception class and ': '
+     * @param string|null $file       - when given, suffix message with ' in ' + path to this file
+     * @param int|null    $line       - when given, suffix file with ':' + line
+     * @param bool        $with_trace - when given, suffix message with stack trace
+     */
+    protected function createLogEntry(
+        $level = 'ERROR',
+        $message = null,
+        $class = null,
+        $file = null,
+        $line = null,
+        $with_trace = false
+    ) {
+        /**
+         * If class is given, prefix it with base path.
+         *  - if message is not null, separate class and message with ': '
+         **/
+        if ($class) {
+            $class = $message ?
+                base_path() . '/' . $class . ': ' :
+                base_path() . '/' . $class;
+        }
+
+        $line  = $line ? ':' . $line : null;
+        $file  = $file ? ' in ' . base_path() . '/' . $file . $line : null;
+        $trace = $with_trace ? ' Stack trace: ' . (new \Exception())->getTraceAsString() : null;
+
+        // assemble log message
+        $log = $class . $message . $file . $trace;
+
+        switch ($level) {
+            case('EMERGENCY') :
+                Log::emergency($log);
+                break;
+
+            case('ALERT') :
+                Log::alert($log);
+                break;
+
+            case('CRITICAL') :
+                Log::critical($log);
+                break;
+
+            case('ERROR') :
+                Log::error($log);
+                break;
+
+            case('WARNING') :
+                Log::warning($log);
+                break;
+
+            case('NOTICE') :
+                Log::notice($log);
+                break;
+
+            case('INFO') :
+                Log::info($log);
+                break;
+
+            case('DEBUG') :
+                Log::debug($log);
+                break;
+
+            default:
+                Log::error($log);
+                break;
+        }
+
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\ModelNotFoundException
+     */
+    protected function logModelNotFoundException()
+    {
+        $e = new ModelNotFoundException();
+        $e->setModel(Bugger::class, 66);
+        Log::error($e);
+
+        return $e;
+    }
 }
